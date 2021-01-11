294c72bba218ea99e4aa4a540655be83d9e1dfe4
Christian Stefanescu <christian.stefanescu@lawpilots.com>
2021-01-11 14:12:40 +0100

    Split into 3 endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::response::status::BadRequest;
+use rand::Rng;
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/?<limit>")]
-fn hello(limit: Option<u32>) -> Result<String, BadRequest<String>> {
-    match limit {
-        Some(l) => Ok(format!("Hello, limit {}!", l)),
-        None => Err(BadRequest(Some(
-            "limit should be an unsigned 32bit integer".to_string(),
-        ))),
-    }
+fn get_rand(from: Option<u32>, to: Option<u32>) -> u32 {
+    let mut rng = rand::thread_rng();
+    let from = match from {
+        Some(f) => f,
+        None => 0,
+    };
+    let to = match to {
+        Some(f) => f,
+        None => 100,
+    };
+    rng.gen_range(from..=to)
+}
+
+#[get("/")]
+fn no_limit() -> String {
+    format!("{}\n", get_rand(Some(0), Some(100)))
+}
+
+#[get("/<to>")]
+fn upper_limit(to: u32) -> String {
+    format!("{}\n", get_rand(Some(0), Some(to)))
+}
+
+#[get("/<from>/<to>")]
+fn both_limits(from: u32, to: u32) -> String {
+    format!("{}\n", get_rand(Some(from), Some(to)))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![no_limit, upper_limit, both_limits])
+        .launch();
 }
