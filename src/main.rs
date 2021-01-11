5ad35b81707c32e9c2bdb11e26557a764d0bf476
Christian Stefanescu <chris@0chris.com>
2021-01-11 22:19:38 +0100

    Use rng as State

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rand::{SeedableRng};
+use rand::rngs::StdRng;
 use rand::Rng;
+
+use rocket::State;
 
 #[macro_use]
 extern crate rocket;
 
-fn get_rand(from: Option<u32>, to: Option<u32>) -> u32 {
-    let mut rng = rand::thread_rng();
+struct Data {
+    rng: StdRng,
+}
+
+fn get_rand(from: Option<u32>, to: Option<u32>, data: State<Data>) -> u32 {
     let from = match from {
         Some(f) => f,
         None => 0,
@@ -15,26 +22,28 @@ fn get_rand(from: Option<u32>, to: Option<u32>) -> u32 {
         Some(f) => f,
         None => 100,
     };
-    rng.gen_range(from..=to)
+    data.rng.clone().gen_range(from..=to)
 }
 
 #[get("/")]
-fn no_limit() -> String {
-    format!("{}\n", get_rand(Some(0), Some(100)))
+fn no_limit(data: State<Data>) -> String {
+    format!("{}\n", get_rand(Some(0), Some(100), data))
 }
 
 #[get("/<to>")]
-fn upper_limit(to: u32) -> String {
-    format!("{}\n", get_rand(Some(0), Some(to)))
+fn upper_limit(to: u32, data: State<Data>) -> String {
+    format!("{}\n", get_rand(Some(0), Some(to), data))
 }
 
 #[get("/<from>/<to>")]
-fn both_limits(from: u32, to: u32) -> String {
-    format!("{}\n", get_rand(Some(from), Some(to)))
+fn both_limits(from: u32, to: u32, data: State<Data>) -> String {
+    format!("{}\n", get_rand(Some(from), Some(to), data))
 }
 
 fn main() {
+    let rng = StdRng::from_entropy();
     rocket::ignite()
         .mount("/", routes![no_limit, upper_limit, both_limits])
+        .manage(Data{rng})
         .launch();
 }
