2cfdc8ccaed86fecf0b55afff2a6200171233bd6
Christian Stefanescu <christian.stefanescu@lawpilots.com>
2021-01-11 11:55:10 +0100

    Add errors

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rocket::response::status::BadRequest;
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/?<limit>")]
-fn hello(limit: Option<u32>) -> String {
-    let limit = limit.unwrap_or(100);
-    format!("Hello, limit {}!", limit)
+fn hello(limit: Option<u32>) -> Result<String, BadRequest<String>> {
+    match limit {
+        Some(l) => Ok(format!("Hello, limit {}!", l)),
+        None => Err(BadRequest(Some(
+            "limit should be an unsigned 32bit integer".to_string(),
+        ))),
+    }
 }
 
 fn main() {
