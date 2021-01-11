c2687fa0b4e54eeac6b16d32901e6f6ab3cc773b
Christian Stefanescu <christian.stefanescu@lawpilots.com>
2021-01-11 11:34:01 +0100

    Add basic rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/?<limit>")]
+fn hello(limit: Option<u32>) -> String {
+    let limit = limit.unwrap_or(100);
+    format!("Hello, limit {}!", limit)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
