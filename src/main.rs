c9a2f4d38230ee4263f58325bcf9aaf038156dea
Christian Stefanescu <chris@0chris.com>
2021-01-12 08:13:24 +0100

    Init RNG with lazy_static

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,42 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::sync::{Mutex};
+
 use rand::{SeedableRng};
 use rand::rngs::StdRng;
-use rand::Rng;
+use rand::{thread_rng, Rng};
 
-use rocket::State;
+use lazy_static::lazy_static;
+
 
 #[macro_use]
 extern crate rocket;
 
-struct Data {
-    rng: StdRng,
+lazy_static! {
+    static ref RNG: Mutex<StdRng> = Mutex::new(StdRng::from_rng(thread_rng()).unwrap());
 }
 
-fn get_rand(from: Option<u32>, to: Option<u32>, data: State<Data>) -> u32 {
-    let from = match from {
-        Some(f) => f,
-        None => 0,
-    };
-    let to = match to {
-        Some(f) => f,
-        None => 100,
-    };
-    data.rng.clone().gen_range(from..=to)
+fn get_rand(from: Option<u32>, to: Option<u32>) -> u32 {
+    RNG.lock().unwrap().gen_range(from.unwrap_or(0)..=to.unwrap_or(100))
 }
 
 #[get("/")]
-fn no_limit(data: State<Data>) -> String {
-    format!("{}\n", get_rand(Some(0), Some(100), data))
+fn no_limit() -> String {
+    format!("{}\n", get_rand(Some(0), Some(100)))
 }
 
 #[get("/<to>")]
-fn upper_limit(to: u32, data: State<Data>) -> String {
-    format!("{}\n", get_rand(Some(0), Some(to), data))
+fn upper_limit(to: u32) -> String {
+    format!("{}\n", get_rand(Some(0), Some(to)))
 }
 
 #[get("/<from>/<to>")]
-fn both_limits(from: u32, to: u32, data: State<Data>) -> String {
-    format!("{}\n", get_rand(Some(from), Some(to), data))
+fn both_limits(from: u32, to: u32) -> String {
+    format!("{}\n", get_rand(Some(from), Some(to)))
 }
 
 fn main() {
-    let rng = StdRng::from_entropy();
     rocket::ignite()
         .mount("/", routes![no_limit, upper_limit, both_limits])
-        .manage(Data{rng})
         .launch();
 }
